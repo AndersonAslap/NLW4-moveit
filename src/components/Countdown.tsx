672017124b660ca072e4dcac5214af3eb65c33fb
AndersonAslap <anderson.adolfo1998@gmail.com>
2021-02-25 22:16:17 -0300

    Lógica de mudar o estado do botão de ciclos

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/components/Countdown.module.css';
 export function Countdown() {
 
     const [time, setTime] = useState(25 * 60);
-    const [active, setActive] = useState(false);
+    const [isActive, setIsActive] = useState(false);
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -14,16 +14,16 @@ export function Countdown() {
     const [secondeLeft, secondeRight] = String(seconds).padStart(2, '0').split('');
 
     function startCountdown() {
-        setActive(true);
+        setIsActive(true);
     }
 
     useEffect(() => {
-        if (active && time > 0) {
+        if (isActive && time > 0) {
             setTimeout(() => {
                 setTime(time - 1);
             }, 1000)
         }
-    }, [active, time]);
+    }, [isActive, time]);
 
     return (
         <div>
@@ -46,7 +46,7 @@ export function Countdown() {
                 className={styles.countdownButton}
                 onClick={startCountdown}
             >
-                Iniciar um ciclo
+                { isActive ? 'Abandonar ciclo' : 'Iniciar um ciclo' }
             </button>
         </div>
     );
